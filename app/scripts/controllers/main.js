591038e55574047a38ee2faa18d88b70952ac1b5
Ruf <bdrf@gft.com>
2017-12-19 14:55:09 +0100

    creating links

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,11 +8,12 @@
  * Controller of the movieManiaApp
  */
 angular.module('movieManiaApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $location) {
 	  $scope.title = 'Thanks for bla';
 	  var movieList = [{
 		  id: 23,
 		  title: 'Ace Drummer',
+		  slug: 'ace-drummer',
 		  image: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjEyNzg3MzcyNF5BMl5BanBnXkFtZTcwNjQ2NTQyMQ@@._V1_.jpg',
 		  description: 'This blabla',
 		  category: 'Epic'
@@ -20,6 +21,7 @@ angular.module('movieManiaApp')
 		{ 
 		  id: 5,
 		  title: 'And Then There Were None',
+		  slug: 'and-then-there-were-none',
 		  image: 'https://images-na.ssl-images-amazon.com/images/M/MV5BYTdkMDg5N2QtOGUzOS00OGY2LTkxMDMtYmU0ZDI5MDMwY2RmXkEyXkFqcGdeQXVyNDA5Mjg5MjA@._V1_UY268_CR6,0,182,268_AL_.jpg',
 		  description: 'Seven guests, a newly hired personal secretary and two staff are gathered on an isolated island by an absent host and someone begins killing them off one by one. They work together to determine who is the killer?',
 		  category: 'Average'
@@ -27,6 +29,7 @@ angular.module('movieManiaApp')
 		{ 
 		  id: 42,
 		  title: 'Captain Kidd',
+		  slug: 'captain-kidd',
 		  image: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMGM5YzUwZmItMDYwOC00ZTY0LTk5ZTMtNzRiNzVlNGFkYTg4L2ltYWdlXkEyXkFqcGdeQXVyNjQzNDI3NzY@._V1_UX182_CR0,0,182,268_AL_.jpg',
 		  description: 'The unhistorical adventures of pirate Captain Kidd revolve around treasure and treachery.',
 		  category: 'Epic'
@@ -42,6 +45,13 @@ angular.module('movieManiaApp')
 			description: '',
 			image: 'https://images-na.ssl-images-amazon.com/images/M/MV5BMjEyNzg3MzcyNF5BMl5BanBnXkFtZTcwNjQ2NTQyMQ@@._V1_.jpg',
 			category: ''
+		};
+		
+		$scope.goToRandomMovie = function(){
+			var index = Math.floor(Math.random() * $scope.movies.length);
+			var movie = $scope.movies[index];
+			var url = 'movie/'+movie.id+'/'+movie.slug;
+			$location.url(url);
 		};
 		
 		$scope.isValid = function(){
